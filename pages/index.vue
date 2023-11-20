f41269c705c541b614df1518a2c3c0cb4b97bb06
emarti2 <eva.martin@bsc.es>
2023-11-20 17:46:41 +0100

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+      </v-card>
+      <v-card>
+        Nuxt Playground 🫶🏻
+        <accessibilityPlot />
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { accessibilityPlot } from 'oeb_visualizations'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    accessibilityPlot
+  }
+}
+</script>
