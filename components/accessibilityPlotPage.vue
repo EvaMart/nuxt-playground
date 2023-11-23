9c4fe69b044e39b4a41f0163fada52f25ab63654
emarti2 <eva.martin@bsc.es>
2023-11-23 16:31:53 +0100

    added citations plot

---
+++ components/accessibilityPlotPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <v-card-subtitle>Access time and status of a service accross time </v-card-subtitle>
+            <accessibilityPlot :dataItems="data" />
+    </div>
+</template>
+<script>
+import { accessibilityPlot } from '@inb/oeb_visualizations'
+import { data } from '../data/uptime30days.js'
+
+export default {
+  name: 'accessibilityPlotPage',
+  components: {
+    accessibilityPlot
+  },
+  data(){
+    return {
+      data: data,
+    }
+  }
+}
+
+</script>
---
+++ components/citationsPlotPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <v-tabs
+        v-model="tabCitations"
+        active-class="white--text"
+        centered
+        dark
+        >
+        <v-tabs-slider color="white"></v-tabs-slider>
+        <v-tab>
+            Simple
+        </v-tab>
+        <v-tab>
+            Stacked
+        </v-tab>
+        <v-tab>
+            Stacked - dark
+        </v-tab>
+        </v-tabs>
+          
+        <v-tabs-items v-model="tabCitations">
+        <v-tab-item>
+            <v-card-subtitle>Number of citations per year</v-card-subtitle>
+            <citationsPlot :dataTraces="dataTraces" :showlegend="true" :colors="colors"/>
+        </v-tab-item>
+        <v-tab-item>
+            <v-card-subtitle>Number of citations per year (stack)</v-card-subtitle>
+            <citationsPlot :dataTraces="dataTraces" :showlegend="true" :colors="colors" :stack="true" />
+        </v-tab-item>
+        <v-tab-item>
+            <v-card-subtitle>Number of citations per year (stack)</v-card-subtitle>
+            <citationsPlot :dataTraces="dataTraces" :showlegend="true" :colors="colors" :stack="true" :darkMode="true" />
+        </v-tab-item>
+        </v-tabs-items>
+    </div>
+</template>
+<script>
+import { citationsPlot } from '@inb/oeb_visualizations'
+
+import { mockData1, mockData2, mockData3 } from '../data/citations10years.js';
+
+export default {
+  name: 'citationsPlotPage',
+  components: {
+    citationsPlot
+  },
+  data(){
+    return {
+      dataTraces: [mockData1, mockData2, mockData3],
+      colors: ['#004c6d','#6b93b1','#c3e1fa'],
+      tabCitations: 0,
+    }
+  }
+}
+</script>
