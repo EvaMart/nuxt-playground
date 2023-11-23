5b39db7992032893299796acaebf7ed3350f1fb6
emarti2 <eva.martin@bsc.es>
2023-11-23 16:31:36 +0100

    added citations plot

--- pages/index.vue
+++ pages/index.vue
@@ -18,12 +18,17 @@
     <v-row justify="center" align="center">
       <v-col cols="12" sm="10" md="10">
         <h2>Components</h2>
+      </v-col>
+      <v-col cols="12" sm="10" md="10">
         <v-card class="pl-8 pr-8 pb-4 mt-4">
           <v-card-title>accessibilityPlot</v-card-title>
-          <v-card-subtitle>Access time and status of a service accross time </v-card-subtitle>
-          
-            <accessibilityPlot :dataItems="data" />
-          
+          <accessibilityPlotPage />
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="10" md="10">
+        <v-card class="pl-8 pr-8 pb-4 mt-4">
+        <v-card-title>citationsPlot</v-card-title>
+          <citationsPlotPage />
         </v-card>
       </v-col>
     </v-row>
@@ -32,19 +37,21 @@
 </template>
 
 <script>
-import { accessibilityPlot } from '@inb/oeb_visualizations'
-//import { accessibilityPlot } from 'oeb_visualizations'
+import accessibilityPlotPage from '../components/accessibilityPlotPage.vue'
+import citationsPlotPage from '../components/citationsPlotPage.vue'
 import { data } from '../data/uptime30days.js'
+
 
 
 export default {
   name: 'IndexPage',
   components: {
-    accessibilityPlot
+    accessibilityPlotPage,
+    citationsPlotPage
   },
   data(){
     return {
-      data: data
+      data: data,
     }
   }
 }
