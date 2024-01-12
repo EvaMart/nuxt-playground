7168a490722a8f80b8cb94ef3816b81ebbcd5aea
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-01-12 13:26:46 +0100

    fix(accessibility): add six months

--- components/accessibilityPlotPage.vue
+++ components/accessibilityPlotPage.vue
@@ -2,10 +2,14 @@
     <div>
         <v-card-subtitle>Access time and status of a service accross time </v-card-subtitle>
           <h5 class="ml-5 mb-4 text-subtitle">One month:</h5>
-            <accessibilityPlot :dataItems="data" :width="width"/>
-            <br>
-            <h5 class="ml-5 mb-4 text-subtitle">One week:</h5>
-            <accessibilityPlot :dataItems="dataWeek" :width="width" week />
+          <accessibilityPlot :dataItems="data" :width="width"/>
+          <br>
+          <h5 class="ml-5 mb-4 text-subtitle">One week:</h5>
+          <accessibilityPlot :dataItems="dataWeek" :width="width" week />
+          <br>
+
+          <h5 class="ml-5 mb-4 text-subtitle">6 months:</h5>
+          <accessibilityPlot :dataItems="dataSixMonths" sixMonths :width="width" />
     </div>
 </template>
 <script>
